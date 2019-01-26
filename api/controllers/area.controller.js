c3d9f68f853db29aadeaa6e670fd9ea953f96447
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2019-01-26 14:50:11 +0000

    update areas routes and controller;

--- api/controllers/area.controller.js
+++ api/controllers/area.controller.js
@@ -5,17 +5,18 @@ const Area = db.areas;
 exports.create = (req, res) => {	
 	// Save to MariaDB database
 	Area.create({  
-			name: req.body.name,
-            areaState: req.body.areaState,
-            owner: req.body.owner
-		})
-		.then(area => {		
-			// Send created area to client
-			res.json(area);
-		})
-		.catch(error => res.status(400).send(error))
+        name: req.body.name,
+        areaState: req.body.areaState,
+        owner: req.body.owner
+	})
+    .then(area => {		
+        // Send created area to client
+        res.json(area);
+    })
+    .catch(error => res.status(400).send(error))
 };
 
+// Get all areas
 exports.findAll = (req, res) => {
     Area.findAll({
         attributes: { exclude: ["createdAt", "updatedAt"] } // I m not sure if I want to remove it from req
@@ -24,4 +25,57 @@ exports.findAll = (req, res) => {
         res.json(areas);
     })
     .catch(error => res.status(400).send(error))
+};
+
+// Find a Area by Id
+exports.findById = (req, res) => {	
+	Area.findById(req.params.areaId,
+        {attributes: { exclude: ["createdAt", "updatedAt"] }}
+    )
+    .then(area => {
+        if (!area){
+            return res.status(404).json({message: "Area Not Found"})
+        }
+
+        return res.status(200).json(area)
+    })
+    .catch(error => res.status(400).send(error));
+};
+
+// Update an Area
+exports.update = (req, res) => {
+	return Area.findById(req.params.areaId)
+		.then(area => {
+            if(!area){
+                return res.status(404).json({
+                    message: 'Area Not Found',
+                });
+            }
+            return area.update({
+                name: req.body.name,
+                areaState: req.body.areaState,
+                owner: req.body.owner
+            })
+            .then(() => res.status(200).json(area))
+            .catch((error) => res.status(400).send(error));
+        })
+		.catch((error) => res.status(400).send(error));			 
+};
+
+// Delete an Area by Id
+exports.delete = (req, res) => {
+	return Area
+        .findById(req.params.areaId)
+        .then(area => {
+            if(!area) {
+                return res.status(400).send({
+                    message: 'Area Not Found',
+                });
+            }
+ 
+            return area.destroy()
+            .then(() => res.status(200).json({message: "Area removed successfully!"}))
+            .catch(error => res.status(400).send(error));
+        })
+        .catch(error => res.status(400).send(error));
 };
--- api/routes/area.route.js
+++ api/routes/area.route.js
@@ -3,8 +3,19 @@
 module.exports = function(app) {
     const areas = require('../controllers/area.controller');
 
-     // Create a new Area
-     app.post('/api/v1/areas/create', areas.create);
+    // Create a new area
+    app.post('/api/v1/areas/create', areas.create);
 
-     app.get('/api/v1/areas', areas.findAll);
+    // Get all areas
+    app.get('/api/v1/areas', areas.findAll);
+
+    // Get specific area
+    app.get('/api/v1/areas/:areaId', areas.findById);
+
+    // Update specific area
+    // Note: possibly to change it from put to patch
+    app.put('/api/v1/areas/:areaId', areas.update);
+
+    // Delete specific area 
+    app.delete('/api/v1/areas/:areaId', areas.delete);
 }
