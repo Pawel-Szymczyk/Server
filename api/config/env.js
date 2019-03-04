bcf9a7b23780152c2446c85c15ad812edff13241
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2019-03-04 01:07:44 +0000

    update db details

--- api/config/env.js
+++ api/config/env.js
@@ -1,17 +1,30 @@
 const env = {
 
-  database: 'home_automation_db',
-    username: 'root',
-    password: 'Welcome123',
-    host: '127.0.0.1',
-    port: 3305,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
+  // database: 'home_automation_db',
+  //   username: 'root',
+  //   password: 'Welcome123',
+  //   host: '127.0.0.1',
+  //   port: 3305,
+  //   dialect: 'mysql',
+  //   pool: {
+  //       max: 5,
+  //       min: 0,
+  //       acquire: 30000,
+  //       idle: 10000
+  //   },
+
+  database: 'gpfpj01cmkdo9jd9',
+  username: 's607azfuqk8lufbv',
+  password: 'p8xq7wdskq137ti4',
+  host: 'axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  dialect: 'mysql',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  },
 
     // mqttHost: 'mqtt://192.168.0.11:1883',
     // mqttUsername: 'MosqAdmin',
