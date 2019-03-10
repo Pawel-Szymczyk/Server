b6f78d8dd1212742d9a94e58a3dc3e085df9c339
Pawel-Szymczyk <38817529+Pawel-Szymczyk@users.noreply.github.com>
2019-03-10 21:03:35 +0000

    update env

--- api/config/env.js
+++ api/config/env.js
@@ -1,40 +1,6 @@
 const env = {
 
-  // database: 'home_automation_db',
-  //   username: 'root',
-  //   password: 'Welcome123',
-  //   host: '127.0.0.1',
-  //   port: 3305,
-  //   dialect: 'mysql',
-  //   pool: {
-  //       max: 5,
-  //       min: 0,
-  //       acquire: 30000,
-  //       idle: 10000
-  //   },
 
-  database: 'gpfpj01cmkdo9jd9',
-  username: 's607azfuqk8lufbv',
-  password: 'p8xq7wdskq137ti4',
-  host: 'axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  dialect: 'mysql',
-  pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-  },
-
-    // mqttHost: 'mqtt://192.168.0.11:1883',
-    // mqttUsername: 'MosqAdmin',
-    // mqttPassword: '9Mosq1TT0pS',
-    mqttHost: 'mqtt://m23.cloudmqtt.com:11023',
-    mqttUsername: 'ugnmeooe',
-    mqttPassword: 'CVPkjCWOrJWU',
-
-    // secretKey: 'th1s1s53cr37k3y'
-    secretKey: 'thisissparta'
 
 };
    
